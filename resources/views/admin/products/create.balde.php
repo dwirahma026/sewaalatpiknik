1f288aca61fd52a194dc04be2f241d9a8ebae5e5
agustinirst <agustinirestiana30@gmail.com>
2024-06-30 22:09:33 +0700

    update product by tini

--- resources/views/admin/products/create.balde.php
+++ resources/views/admin/products/create.balde.php
@@ -1,1 +1,117 @@
-/untuk from manambah produk
+/untuk from manambah produk<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SELAPIK</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-gray-100">
+<div class="flex">
+    <div class="bg-lime-600 w-80 h-dvh flex flex-col font-bold text-3xl">
+        <div class="mt-80 ml-6">
+            <div class="flex">
+                <svg width="50" height="50" viewBox="0 0 65 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M30.5852 7.82087C31.0931 7.37563 31.7818 7.1255 32.5 7.1255C33.2181 7.1255 33.9069 7.37563 34.4148 7.82087L50.6648 22.0709L56.0814 26.8209C56.5748 27.2688 56.8478 27.8687 56.8416 28.4914C56.8354 29.1142 56.5506 29.7098 56.0485 30.1502C55.5463 30.5905 54.867 30.8403 54.1569 30.8457C53.4468 30.8511 52.7627 30.6117 52.2519 30.1791L51.4583 29.4832V45.125C51.4583 46.3848 50.8876 47.593 49.8718 48.4838C48.856 49.3745 47.4782 49.875 46.0417 49.875H37.9167C37.1984 49.875 36.5095 49.6248 36.0016 49.1794C35.4937 48.734 35.2083 48.1299 35.2083 47.5V40.375H29.7917V47.5C29.7917 48.1299 29.5063 48.734 28.9984 49.1794C28.4905 49.6248 27.8016 49.875 27.0833 49.875H18.9583C17.5217 49.875 16.144 49.3745 15.1282 48.4838C14.1123 47.593 13.5417 46.3848 13.5417 45.125V29.4832L12.7481 30.1791C12.2373 30.6117 11.5532 30.8511 10.8431 30.8457C10.1329 30.8403 9.45366 30.5905 8.95152 30.1502C8.44937 29.7098 8.16454 29.1142 8.15837 28.4914C8.1522 27.8687 8.42518 27.2688 8.91853 26.8209L14.3352 22.0709L30.5852 7.82087Z" fill="#1B5E20"/>
+                    </svg>Dashboard
+            </div>
+            <div class="flex mt-3">
+                <svg width="50" height="50" viewBox="0 0 65 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21.125 17.1C21.125 18.2338 20.6114 19.3212 19.6971 20.1229C18.7829 20.9246 17.5429 21.375 16.25 21.375C14.9571 21.375 13.7171 20.9246 12.8029 20.1229C11.8886 19.3212 11.375 18.2338 11.375 17.1C11.375 15.9662 11.8886 14.8788 12.8029 14.0771C13.7171 13.2754 14.9571 12.825 16.25 12.825C17.5429 12.825 18.7829 13.2754 19.6971 14.0771C20.6114 14.8788 21.125 15.9662 21.125 17.1ZM21.125 28.5C21.125 29.6338 20.6114 30.7212 19.6971 31.5229C18.7829 32.3246 17.5429 32.775 16.25 32.775C14.9571 32.775 13.7171 32.3246 12.8029 31.5229C11.8886 30.7212 11.375 29.6338 11.375 28.5C11.375 27.3662 11.8886 26.2788 12.8029 25.4771C13.7171 24.6754 14.9571 24.225 16.25 24.225C17.5429 24.225 18.7829 24.6754 19.6971 25.4771C20.6114 26.2788 21.125 27.3662 21.125 28.5ZM21.125 39.9C21.125 41.0338 20.6114 42.1212 19.6971 42.9229C18.7829 43.7246 17.5429 44.175 16.25 44.175C14.9571 44.175 13.7171 43.7246 12.8029 42.9229C11.8886 42.1212 11.375 41.0338 11.375 39.9C11.375 38.7662 11.8886 37.6788 12.8029 36.8771C13.7171 36.0754 14.9571 35.625 16.25 35.625C17.5429 35.625 18.7829 36.0754 19.6971 36.8771C20.6114 37.6788 21.125 38.7662 21.125 39.9Z" fill="#1B5E20"/>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M24.375 17.1C24.375 16.3441 24.7174 15.6192 25.3269 15.0847C25.9364 14.5503 26.763 14.25 27.625 14.25H50.375C51.237 14.25 52.0636 14.5503 52.6731 15.0847C53.2826 15.6192 53.625 16.3441 53.625 17.1C53.625 17.8559 53.2826 18.5808 52.6731 19.1153C52.0636 19.6497 51.237 19.95 50.375 19.95H27.625C26.763 19.95 25.9364 19.6497 25.3269 19.1153C24.7174 18.5808 24.375 17.8559 24.375 17.1ZM24.375 28.5C24.375 27.7441 24.7174 27.0192 25.3269 26.4847C25.9364 25.9503 26.763 25.65 27.625 25.65H50.375C51.237 25.65 52.0636 25.9503 52.6731 26.4847C53.2826 27.0192 53.625 27.7441 53.625 28.5C53.625 29.2559 53.2826 29.9808 52.6731 30.5153C52.0636 31.0497 51.237 31.35 50.375 31.35H27.625C26.763 31.35 25.9364 31.0497 25.3269 30.5153C24.7174 29.9808 24.375 29.2559 24.375 28.5ZM24.375 39.9C24.375 39.1441 24.7174 38.4192 25.3269 37.8847C25.9364 37.3503 26.763 37.05 27.625 37.05H50.375C51.237 37.05 52.0636 37.3503 52.6731 37.8847C53.2826 38.4192 53.625 39.1441 53.625 39.9C53.625 40.6559 53.2826 41.3808 52.6731 41.9153C52.0636 42.4497 51.237 42.75 50.375 42.75H27.625C26.763 42.75 25.9364 42.4497 25.3269 41.9153C24.7174 41.3808 24.375 40.6559 24.375 39.9Z" fill="#1B5E20"/>
+                    </svg> Produk                
+            </div>
+            <div class="flex mt-3">
+                <svg width="50" height="50" viewBox="0 0 65 57" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M24.375 19C24.375 17.1103 25.231 15.2981 26.7547 13.9619C28.2785 12.6257 30.3451 11.875 32.5 11.875C34.6549 11.875 36.7215 12.6257 38.2452 13.9619C39.7689 15.2981 40.625 17.1103 40.625 19H24.375ZM18.9583 19C18.9583 15.8506 20.385 12.8301 22.9246 10.6031C25.4641 8.37611 28.9085 7.125 32.5 7.125C36.0914 7.125 39.5358 8.37611 42.0754 10.6031C44.6149 12.8301 46.0416 15.8506 46.0416 19H54.1666C54.5429 19 54.915 19.0687 55.2592 19.2018C55.6035 19.3349 55.9124 19.5294 56.1661 19.7729C56.4199 20.0165 56.6131 20.3037 56.7332 20.6164C56.8534 20.929 56.898 21.2602 56.8641 21.5888L54.6027 43.396C54.4189 45.1662 53.4872 46.8123 51.9904 48.0111C50.4936 49.2098 48.5399 49.8746 46.5129 49.875H18.4871C16.46 49.8746 14.5063 49.2098 13.0096 48.0111C11.5128 46.8123 10.5811 45.1662 10.3973 43.396L8.13581 21.5888C8.10195 21.2602 8.14653 20.929 8.2667 20.6164C8.38688 20.3037 8.58001 20.0165 8.8338 19.7729C9.08759 19.5294 9.39645 19.3349 9.74072 19.2018C10.085 19.0687 10.4571 19 10.8333 19H18.9583Z" fill="#1B5E20"/>
+                    </svg> Pesanan                
+            </div>
+        </div>
+    </div>
+    <div class="bg-lime-400 w-full h-20">
+        <div class="mt-7 ml-3 font-bold text-4xl">
+            <h1>Tambah Produk</h1>
+        </div>
+        <div>
+            <button class="flex items-center bg-gray-300 w-32 h-16 font-bold text-center text-lg rounded-lg ml-10 mt-10 shadow-lg hover:shadow-xl transition duration-500">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-8 h-8 ml-3">
+                    <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-4.28 9.22a.75.75 0 0 0 0 1.06l3 3a.75.75 0 1 0 1.06-1.06l-1.72-1.72h5.69a.75.75 0 0 0 0-1.5h-5.69l1.72-1.72a.75.75 0 0 0-1.06-1.06l-3 3Z" clip-rule="evenodd" />
+                  </svg>Kembali</button>
+        </div>
+        <div class="flex flex-col items-center mt-10">
+            <div class="bg-yellow-100 w-4/5 px-4 py-5 shadow-2xl mt-10">
+                <div>
+                    <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
+
+                        @csrf
+
+                    <div class="form-group mb-3">
+                        <label class="font-bold">Nama :</label>
+                        <input type="text" class="form-control @error('name') is-invalid @enderror w-96 h-10 border-black border-t-2 border-b-2 border-l-2 border-r-2" name="name" value="{{ old('name') }}" placeholder="Masukkan Nama Product">
+                    
+                        @error('name')
+                            <div class="alert alert-denger mt-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group mb-3">
+                        <label class="font-bold">stok :</label>
+                        <input type="number" class="form-control @error('stok') is-invalid @enderror w-80 h-10 border-black border-t-2 border-b-2 border-l-2 border-r-2">
+                    
+                        @error('stok')
+                            <div class="alert alert-denger mt-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group mb-3">
+                        <label class="font-bold">Harga :</label>
+                        <input type="number" class="form-control @error('price') is-invalid @enderror w-80 h-10 border-black border-t-2 border-b-2 border-l-2 border-r-2" name="price" value="{{ old('price') }}" placeholder="Masukkan Harga Produk">
+                    
+                        @error('price')
+                            <div class="alert alert-denger mt-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group mb-3">
+                        <label class="font-bold">Deskripsi :</label>
+                        <textarea class="form-control @error('deskription') in-invalid @enderror w-80 h-20 border-black border-t-2 border-b-2 border-l-2 border-r-2" name="deskription" rows="5" placeholder="Masukkan Deskripsi Produk">{{ old('deskription') }}</textarea>
+                        
+                        @error('deskription')
+                            <div class="alert alert-denger mt-2">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                    <div class="form-group mb-3">
+                        <label>
+                            <svg width="100" height="100" viewBox="0 0 143 134" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M17.875 117.25V16.75H83.4167V27.9167H29.7917V106.083H113.208V55.8333H125.125V117.25H17.875ZM101.292 50.25V39.0833H89.375V27.9167H101.292V16.75H113.208V27.9167H125.125V39.0833H113.208V50.25H101.292ZM35.75 94.9167H107.25L84.9062 67L67.0312 89.3333L53.625 72.5833L35.75 94.9167ZM29.7917 61.4167V106.083V27.9167V61.4167Z" fill="#545454"/>
+                                </svg></label>
+                                <input type="file" class="form-control @error('photo') in-invalid @enderror" id="photo" name="photo">
+
+                        @error('photo')
+                            <div class="alert alert-denger mt-2">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                    </div>
+                    <div class="ml-10 mt-10 mb-5">
+                        <button type="submit" class="bg-yellow-200 h-10 w-28 flex items-center font-bold rounded-l-2xl rounded-r-2xl shadow-lg hover:shadow-xl transition duration-500">
+                            <svg width="30" height="30" class="ml-2" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M19 33.25C16.3812 33.25 14.25 31.1188 14.25 28.5L14.3339 23.6661L9.5285 23.75C6.88117 23.75 4.75 21.6188 4.75 19C4.75 16.3812 6.88117 14.25 9.5 14.25L14.3339 14.1645L14.25 9.5285C14.25 6.88117 16.3812 4.75 19 4.75C21.6188 4.75 23.75 6.88117 23.75 9.5L23.8371 14.1645L28.5285 14.25C31.1188 14.25 33.25 16.3812 33.25 19C33.25 21.6188 31.1188 23.75 28.5 23.75L23.8371 23.6661L23.75 28.5285C23.75 31.1188 21.6188 33.25 19 33.25ZM17.4167 20.5833V28.5285C17.4167 29.3724 18.1276 30.0833 19 30.0833C19.8724 30.0833 20.5833 29.3724 20.5833 28.5V20.5833H28.5285C29.3724 20.5833 30.0833 19.8724 30.0833 19C30.0833 18.1276 29.3724 17.4167 28.5 17.4167H20.5833V9.5C20.5833 8.59908 19.8724 7.91667 19 7.91667C18.1276 7.91667 17.4167 8.62758 17.4167 9.5V17.4167H9.5C8.59908 17.4167 7.91667 18.1276 7.91667 19C7.91667 19.8724 8.62758 20.5833 9.5 20.5833H17.4167Z" fill="black"/>
+                                </svg> Tambah                                                
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    CKEDITOR.replace('deskription');
+</script>
+</body>
+</html>
